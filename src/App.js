d048d9af284d1e38eff383b4f47ba1a5737c4ac6
brunoPedrosa <brunofellipep@gmail.com>
2020-09-04 18:45:59 -0300

    f

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import Homepage from "./pages/homepage/Homepage";
 import JoinPage from "./pages/join-page/JoinPage";
@@ -16,18 +16,18 @@ import NavMobile from "./components/nav-mobile/NavMobile";
 const App = () => {
   return (
     <div className="Container">
-      <Banner imageUrl="https://i.pinimg.com/564x/91/a6/23/91a6233ced3799f85ed93326af0f238e.jpg" />
-      <Header />
-      <NavMobile />
-      <Switch>
+      <Router>
+        <Banner imageUrl="https://i.pinimg.com/564x/91/a6/23/91a6233ced3799f85ed93326af0f238e.jpg" />
+        <Header />
+        <NavMobile />
+          <Redirect from="/" to="home" />
           <Route path="/home" component={Homepage} />
           <Route path="/join" component={JoinPage} />
           <Route path="/about" component={AboutPage} />
           <Route path="/sign-in" component={SignInPage} />
           <Route path="/shop" component={ShopPage} />
           <Route path="/cart/:id" component={CartPage} />
-        
-      </Switch>
+      </Router>
     </div>
   );
 };
